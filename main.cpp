426f36d0c2cd0a759238784e996df98bc0020711
Thomas Grice <augustus.grice@gmail.com>
2015-10-01 15:10:34 -0500

    main changes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+//flow for cpu
+// fetch
+// check if operand
+// if so fetch operand as wel
+// decode
+// load
+// 2nd load ONLY for ind load, interrupt return,
+// execute
+// store
+// interrupt
+
 int main() {
     cout << "Hello, World!" << endl;
     CPU cpu = CPU();
